03d6a7ea246e437b19a1f171eef000354fe96fe4
Chris Moody <cmoody.eit@gmail.com>
2015-07-29 19:25:53 -0700

    fix active record to only include emittable methods on emittable models

--- lib/material_raingular/websocket/active_record_extension.rb
+++ lib/material_raingular/websocket/active_record_extension.rb
@@ -7,15 +7,13 @@ module EmittableExtension
     def emittable
       @emittable = true
       after_save :emit_changes
+      define_method(:emit_changes) {WebsocketRails.users.each{|user| user.send_message :change, websocket_message, namespace: websocket_namespace}}
+      define_method(:websocket_message)   {self.as_json}
+      define_method(:websocket_namespace) {"#{self.class.name.underscore}_#{self.id}".to_sym}
     end
     def emittable?() @emittable || false end
   end
   def emittable?() self.class.emittable? end
-  private
-  def emit_changes
-    namespace = "#{self.class.name.underscore}_#{self.id}".to_sym
-    WebsocketRails.users.each{|user| user.send_message :change, self.as_json, namespace: namespace}
-  end
 end
 module UserManagerExtension
   def to_active_record() User.where(id: @users.keys.reject(&:blank?)) end
